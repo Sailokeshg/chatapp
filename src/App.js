d0e3d1f8347833d6f2af55f0066da5b4396b55b7
sailokeshg <18r01a0579@cmritonline.ac.in>
2021-08-18 12:30:26 +0530

    ChatAPI initialised with single user

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <ChatEngine
+         height="100vh"
+         projectID="925381d0-aeb8-4b63-ae8c-6f876f1dbd2b"
+         userName="john"
+         userSecret="qwerty"
+      />
+    
     </div>
   );
 }
