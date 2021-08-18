23db8af30d1b51dc3c1941fe29d388e2e0f9ddea
sailokeshg <18r01a0579@cmritonline.ac.in>
2021-08-18 12:53:14 +0530

    Components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
-
+import ChatFeed from './components/ChatFeed';
 function App() {
   return (
     <div className="App">
@@ -9,6 +9,8 @@ function App() {
          projectID="925381d0-aeb8-4b63-ae8c-6f876f1dbd2b"
          userName="john"
          userSecret="qwerty"
+         renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+         onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
       />
     
     </div>
